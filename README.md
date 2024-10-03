7d7245f83c942a897f73d769580cfdb703f7aea4
Eyrian Muet <eyrianmuet@gmail.com>
2024-10-03 14:02:24 +1100

    Ajout de commentaires à server-express.mjs et d'une petite description dans le README

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # MUET_Eyrian_CC4
+
+https://muet-eyrian-cc4.onrender.com
+
+Service de raccourcissement d'URL en utilisant Node.js, Express et EJS.
+Le service permet aux utilisateurs de soumettre une longue URL et d'obtenir une version raccourcie, qu'ils peuvent utiliser pour rediriger vers le lien d'origine.
+
+- Empêche de dupliquer plusieurs fois la même url
+
+- Enregistre les liens dans un fichier JSON
+
+- Génére automatiquement des liens courts uniques
--- server-express.mjs
+++ server-express.mjs
@@ -4,7 +4,7 @@ import createError from "http-errors";
 import crypto from "crypto";
 import fs from "fs/promises";
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 10000;
 const host = "0.0.0.0";
 
 const app = express();
@@ -14,13 +14,16 @@ if (app.get("env") === "development") app.use(morgan("dev"));
 app.set("view engine", "ejs");
 
 app.use(express.static("static"));
+// On utilise express.urlencoded pour traiter les requêtes POST
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Fonction qui va générer les identifiants courts
 function generateShortId() {
     return crypto.randomBytes(3).toString('hex');
 }
 
+// Fonction pour récupérer les infos dans links.json
 async function readLinksJSON() {
     try {
         const data = await fs.readFile("./static/links.json", "utf-8"); 
@@ -31,6 +34,7 @@ async function readLinksJSON() {
     }
 }
 
+// Fonction pour écrire dans links.json
 async function writeLinksJSON(linksJSON) {
     try {
         const data = JSON.stringify(linksJSON, null, 4);
@@ -45,16 +49,22 @@ app.get("/", async function (request, response, next) {
 });
 
 app.get("/accueil", async function (request, response, next) {
+    /* On gère l'affichage de la page d'accueil :
+        - S'il n'y a pas de lien dans l'URL on affiche la page d'accueil normalement 
+        - Sinon on affichera le lien dans une box en plus en-dessous
+    */
     const link = request.query.link;
     return response.render("accueil", { link });
 });
 
 app.post("/shortenLink", async function (request, response, next) {
+    //Cette partie s'active lorsque l'utilisateur demande à réduire une url avant de renvoyer sur la page d'accueil
     const originalLink = request.body.url;
 
     let linksJSON = await readLinksJSON();
     let finalLink;
     let lienExistant = false;
+    // Si le lien à transformer est déjà associé à un lien réduit on va donner le lien réduit directement au lieu de créer plusieurs liens réduits pour une même url
     for (let key in linksJSON) {
         if (linksJSON[key] === originalLink) {
             lienExistant = true;
@@ -63,6 +73,7 @@ app.post("/shortenLink", async function (request, response, next) {
         }
     }
 
+    // Si le lien n'est pas déjà associé à un lien réduit on va l'ajouter dans links.json
     if (!lienExistant) {
         const shortLink = generateShortId();
         finalLink = `${request.protocol}://${request.get('host')}/${shortLink}`;
@@ -74,17 +85,22 @@ app.post("/shortenLink", async function (request, response, next) {
         await writeLinksJSON(linksJSON);
     }
 
+    // On redirect vers la page d'accueil en mettant en paramètre le lien créé
     return response.redirect(`/accueil?link=${encodeURIComponent(finalLink)}`);
 });
 
 app.get("/:shortLink", async function (request, response, next) {
+    //Cette partie sert à rediriger une requête vers un lien réduit vers le lien original
     const shortLink = request.params.shortLink;
 
     let linksJSON = await readLinksJSON();
+    // Si le lien réduit existe on redirige vers la page associée
     try {
         const originalLink = linksJSON[`${request.protocol}://${request.get('host')}/${shortLink}`];
         return response.redirect(originalLink);
-    } catch (error) {
+    }
+    //Sinon on retourne une erreur
+    catch (error) {
         console.error(error);
         return next(createError(404));
     }
